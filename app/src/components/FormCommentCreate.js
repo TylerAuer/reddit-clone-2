9cc37002b6c08f96d3c4a9a26c633f389190d1dc
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-03 07:29:33 -0700

    add semantic styles to leave comment form

--- app/src/components/FormCommentCreate.js
+++ app/src/components/FormCommentCreate.js
@@ -1,64 +1,7 @@
-/** @jsx jsx */
 import React from 'react';
-import { css, jsx } from '@emotion/core';
-import { COLORS } from '../constants';
+import { Form, Button } from 'semantic-ui-react';
 import { LoginContext } from '../contexts/LoginContext';
-import BtnBlue from './BtnBlue';
 import createComment from '../functions/createComment';
-
-const postStyles = css`
-  padding-top: 3rem;
-  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-  h2 {
-    text-transform: uppercase;
-    margin-bottom: 2.5rem;
-    font-size: 5rem;
-    display: inline-block;
-    padding-bottom: 1rem;
-    border-bottom: 2px solid ${COLORS['green-light']};
-  }
-
-  label {
-    color: ${COLORS.tan};
-    display: block;
-    font-size: 2.25rem;
-  }
-
-  input,
-  textarea {
-    width: 100%;
-    font-size: 2rem;
-    font-weight: 500;
-    padding: 1rem;
-    background: white;
-    color: #555555;
-    border: 2px solid ${COLORS['green-light']};
-    border-radius: 1rem;
-    margin-bottom: 2rem;
-    outline: none;
-    font-family: 'Lato';
-  }
-
-  textarea {
-    width: 100%;
-    height: 400px;
-    padding: 1rem;
-  }
-
-  input::placeholder,
-  textarea::placeholder {
-    color: ${COLORS['green-xdark']};
-    opacity: 0.25;
-    font-weight: normal;
-    text-shadow: none;
-  }
-
-  input:focus,
-  textarea:focus {
-    border-color: ${COLORS.orange};
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-  }
-`;
 
 const FormCommentCreate = (props) => {
   const [loginState] = React.useContext(LoginContext);
@@ -79,8 +22,8 @@ const FormCommentCreate = (props) => {
   };
 
   return (
-    <div css={postStyles}>
-      <form onSubmit={submit} id="formId">
+    <Form onSubmit={submit} id="formId">
+      <Form.Field>
         <label htmlFor="comment_body">
           <b>Leave a comment: </b>
 
@@ -92,12 +35,11 @@ const FormCommentCreate = (props) => {
             onChange={handleFormChange}
           />
         </label>
-        <br />
-        <BtnBlue onClick={() => null} type="submit">
-          Submit this comment!
-        </BtnBlue>
-      </form>
-    </div>
+      </Form.Field>
+      <Button onClick={submit} type="submit">
+        Submit this comment!
+      </Button>
+    </Form>
   );
 };
 
