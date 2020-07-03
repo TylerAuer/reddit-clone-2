8af30673235aecfdc1bb824ff3ad360519bceadc
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-03 12:23:12 -0700

    delete unused BtnWhite and BtnBlue components

--- app/src/components/BtnWhite.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
-
-const BtnWhite = (props) => {
-  return <Button onClick={() => props.onClick()}>{props.children}</Button>;
-};
-
-export default BtnWhite;
