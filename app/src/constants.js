b340522895ab4d5f6de7652bb20b076814e230e7
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-08 12:09:26 -0700

    clean up unused files and constants

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,28 +1,3 @@
-const FEATURES = {
-  FEED: 0,
-  // User CRUD
-  USER_CREATE: 1,
-  USER_READ: 2,
-  USER_UPDATE: 3,
-  USER_DELETE: 4,
-  USER_LOGIN_MODAL: 5,
-  // Post CRUD
-  POST_CREATE: 11,
-  POST_READ: 12,
-  POST_UPDATE: 13,
-  POST_DELETE: 14,
-  // Comment CRUD
-  COMMENT_CREATE: 21,
-  COMMENT_READ: 22,
-  COMMENT_UPDATE: 23,
-  COMMENT_DELETE: 24,
-  // Like CRUD
-  LIKE_CREATE: 31,
-  LIKE_READ: 32,
-  LIKE_UPDATE: 33,
-  LIKE_DELETE: 34,
-};
-
 const COLORS = {
   blue: '#05386B',
   'green-xdark': '#005443',
@@ -39,4 +14,4 @@ const STYLES = {
   font: "'Open Sans', sans-serif",
 };
 
-export { FEATURES, COLORS, STYLES };
+export { COLORS, STYLES };
