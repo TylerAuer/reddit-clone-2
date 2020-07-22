67453c2823eb86547a1e1e3c5c5cca9335633f35
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-22 14:05:23 -0700

    set up passport-local and confirm correct use

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const setupPassport = require('./passport/setupPassport');
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jsonParser = bodyParser.json();
+
 const accountRoutes = require('./routes/accountRoutes');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
@@ -12,16 +18,45 @@ const heartRoutes = require('./routes/heartRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'cats',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(jsonParser);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.setMaxListeners(20); // Default: 10 (Helps spot emitter memory leaks)
 
-const port = 4000;
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+///////////////////////////////////////////////
+// PASSPORT CONFIG
+setupPassport(app);
+
+// PASSPORT TEST
+app.post('/API/account/signin', passport.authenticate('local'), (req, res) => {
+  // If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  const user = { ...req.user.dataValues };
+  delete user.password;
+  res.send(user);
+});
+
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/');
+};
 
 ///////////////////////////////////////////////
 // ENDPOINTS
 
 // ACCOUNTS
-app.post('/API/account/signin', accountRoutes.signIn);
+// app.post('/API/account/signin', accountRoutes.signIn);
 // app.get('/API/account/signout', accountRoutes.signout);
 app.post('/API/account/create', accountRoutes.signUp);
 // app.patch('/API/account/update', userRoutes.updateUserAccountInfo);
@@ -55,3 +90,9 @@ app.delete('/API/comment/', commentRoutes.deleteComment);
 // HEART
 app.post('/API/heart', heartRoutes.addHeart);
 app.delete('/API/heart', heartRoutes.removeHeart);
+
+///////////////////////////////////////////////
+// SPIN UP
+
+const port = 4000;
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
