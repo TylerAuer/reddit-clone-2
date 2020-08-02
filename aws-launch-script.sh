3f3f7d938092abd177bb3df7488811f1521ae7e7
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-02 13:21:56 -0700

    copy launch script and change external port to 80 in docker-compose

---
+++ aws-launch-script.sh
@@ -0,0 +1,8 @@
+# get my launch script from my public repo
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/TylerAuer/reddit-clone-2/master/lightsail-compose.sh
+
+# change permissions so the file can be run
+chmod +x ./lightsail-compose.sh
+
+# run the launch script
+./lightsail-compose.sh
