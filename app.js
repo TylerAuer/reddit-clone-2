6869cc3ba8cf6c309e30b2266b28784a4a9282e0
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-14 09:40:28 -0700

    set app.listen to env.variable if exists for heroku

--- app.js
+++ app.js
@@ -108,5 +108,5 @@ app.delete('/API/heart', isAuthenticated, heartRoutes.removeHeart);
 ///////////////////////////////////////////////
 // SPIN UP
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
