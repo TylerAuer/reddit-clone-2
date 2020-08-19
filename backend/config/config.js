d21ae784becd2972e9a778c81732022b8978c2d2
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-19 10:17:12 -0700

    reconfig local dev setup which I broke when configuring Heroku

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,18 @@
 module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: null,
+    database: process.env.POSTGRES_DB,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: false,
+  },
   production: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     port: process.env.POSTGRES_PORT,
-    host: process.env.DATABASE_URL || 'postgres',
+    host: process.env.DATABASE_URL || '127.0.0.1',
     dialect: 'postgres',
     operatorsAliases: 0,
     logging: false,
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,13 +1,14 @@
 'use strict';
-
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+console.log(config);
 let sequelize;
 
 // checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
